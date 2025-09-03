f1193ab58b06e77aec99acfd8b9f9d4d7a29be84
LaPapostia <juanes041387@gmail.com>
2025-09-03 15:44:24 -0500

    #8 Creating the angular project and the first interaction with the backend

---
+++ angular_app_payments/src/app/auth/login/login.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.loginForm = this.fb.group({
+      correo: ['', [Validators.required, Validators.email]],
+      contrasenia: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    const { correo, contrasenia } = this.loginForm.value;
+
+    this.authService.login(correo, contrasenia).subscribe({
+      next: (res) => {
+        console.log('Login exitoso:', res);
+        alert('Bienvenido ' + res.usuario);
+      },
+      error: (err) => {
+        console.error('Error en login:', err);
+
+        if (err.status === 401) {
+          alert('Credenciales incorrectas');
+        } else if (err.status === 500) {
+          alert('Error interno del servidor: ' + (err.error || 'Intenta nuevamente más tarde'));
+        } else {
+          alert('Error en el login: ' + (err.error?.message || err.message));
+        }
+      }
+    });
+  }
+
+}
---
+++ angular_app_payments/src/app/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'https://localhost:7269/api/User'; // tu API backend
+
+  constructor(private http: HttpClient) {}
+
+  login(correo: string, contrasenia: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { correo, contrasenia });
+  }
+}
